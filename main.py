2e5073a395ab8f5be53a848ddb1f19346967f9ad
Hope Tambala <hopetambala@outlook.com>
2019-01-10 17:50:14 -0500

    update()

--- main.py
+++ main.py
@@ -7,30 +7,34 @@ from bokeh.io import output_file, show
 from bokeh.models import HoverTool, ColumnDataSource
 from bokeh.plotting import figure
 
-df = pandas_get_teams()
-'''
-Columns
-['Id', 'Name', 'Nickname', 'Squad', 'AverageAge', 'NumberofForeigners', 'TotalMarketValue', 'AverageMarketValue', 'League']
-'''
-print(df.columns.tolist())
+def scatter_foreigners_totalMarketValue():
+    df = pandas_get_teams()
+    '''
+    Columns
+    ['Id', 'Name', 'Nickname', 'Squad', 'AverageAge', 'NumberofForeigners', 'TotalMarketValue', 'AverageMarketValue', 'League']
+    '''
 
-source = ColumnDataSource(df)
+    source = ColumnDataSource(df)
 
-p = figure()
-p.circle(x='NumberofForeigners', y='TotalMarketValue', 
-         source=source, 
-         size=10, color='green')
+    p = figure()
+    p.circle(x='NumberofForeigners', y='TotalMarketValue', 
+            source=source, 
+            size=10, color='green')
 
-p.title.text = 'Number of Foreign League Players and Total Market Value'
-p.xaxis.axis_label = 'NumberofForeigners'
-p.yaxis.axis_label = 'TotalMarketValue'
+    p.title.text = 'Number of Foreign League Players and Total Market Value'
+    p.xaxis.axis_label = 'NumberofForeigners'
+    p.yaxis.axis_label = 'TotalMarketValue'
 
-hover = HoverTool()
-hover.tooltips=[
-    ('Name', '@Name'),
-    ('Average Age', '@AverageAge'),
-    ('League', '@League')]
+    hover = HoverTool()
+    hover.tooltips=[
+        ('Name', '@Name'),
+        ('Average Age', '@AverageAge'),
+        ('League', '@League')]
 
-p.add_tools(hover)
+    p.add_tools(hover)
 
-show(p)
+    output_file("graphs/scatterPlot.html")
+
+    show(p)
+
+scatter_foreigners_totalMarketValue()
